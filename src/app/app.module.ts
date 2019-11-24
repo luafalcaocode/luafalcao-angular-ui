9713aaf1f5efcae10a3a3d6d545731c38ce54474
Luã Falcão <falcaodeveloper@outlook.com>
2019-11-24 14:34:16 -0300

    added a module for my home page and refactoring existing code to be more cleaner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
