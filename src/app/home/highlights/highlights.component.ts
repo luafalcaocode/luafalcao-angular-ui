591c2d9ad93a5ea1f3df9d80917adb129e166f86
Luã Falcão <falcaodeveloper@outlook.com>
2019-12-15 01:03:13 -0300

    Included autoplay at slide of images

--- src/app/home/highlights/highlights.component.ts
+++ src/app/home/highlights/highlights.component.ts
@@ -14,27 +14,46 @@ export class HighlightsComponent implements OnInit {
 
   constructor() {
     this.slides = [
-      new SlideViewModel(`Design Patterns #6: Abstract Factory`, `Caindo na categoria de Padrões Criacionais o poderoso Abstract Factory é uma ferramenta muito útil para ajudar  na portabilidade.`, `highlight_1.png`, 1),
-      new SlideViewModel(`Slide 2`, 'Conteúdo do Slide 2', 'highlight_2.png', 2),
-      new SlideViewModel(`Slide 3`, 'Conteúdo do Slide 3', 'highlight_3.png', 3)
+      new SlideViewModel(`Design Patterns #6: Abstract Factory`, `Caindo na categoria de Padrões Criacionais o poderoso Abstract Factory é uma ferramenta muito útil para ajudar  na portabilidade.`, `highlight_1.png`, 0),
+      new SlideViewModel(`Slide 2`, 'Conteúdo do Slide 2', 'highlight_2.png', 1),
+      new SlideViewModel(`Slide 3`, 'Conteúdo do Slide 3', 'highlight_3.png', 2)
     ];
 
-    
+
     this.slideSelectedContent = this.slides[0];
   }
 
   ngOnInit() {
+    this.autoplaySlide();
   }
 
   changeSlide(id: number) {
     let slide = this.slides.filter(slide => slide.id == id)[0];
-    this.slideSelectedContent  = slide;
+    this.slideSelectedContent = slide;
+    this.toggleActiveSlideClass(id.toString());
+  }
 
+  autoplaySlide() {
+    let atual: number = 1;
+    let interval = setInterval(() => {
+      this.slideSelectedContent = this.slides[atual];
+      this.toggleActiveSlideClass(atual.toString());
+      atual++;
+      if (atual == this.slides.length) {
+       clearInterval(interval);
+      }
+    }, 5000);
+  }
+
+  toggleActiveSlideClass(id: string) {
     let slides = document.getElementsByClassName('slide-navigation-action');
     slides[0].classList.remove('active-slide-navigation');
     slides[1].classList.remove('active-slide-navigation');
     slides[2].classList.remove('active-slide-navigation');
 
-    document.getElementById(id.toString()).classList.add('active-slide-navigation');
+    let navigation = document.getElementById(id);
+    if (navigation != undefined) {
+      navigation.classList.add('active-slide-navigation');
+    }
   }
 }
