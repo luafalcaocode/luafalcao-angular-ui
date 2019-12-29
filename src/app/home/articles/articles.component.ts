d2faefc919194cb0eed9157906f63ecacce22c0f
Luã Falcão <falcaodeveloper@outlook.com>
2019-12-28 21:06:58 -0300

    Passing viewmodel to articles component to exibition

--- src/app/home/articles/articles.component.ts
+++ src/app/home/articles/articles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleViewModel } from '../../viewModels/article.viewModel';
 
 @Component({
   selector: 'app-articles',
@@ -6,8 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
+  public articles: ArticleViewModel[];
+  public selectedArticle: ArticleViewModel;
 
-  constructor() { }
+  constructor() {
+    this.selectedArticle =
+      new ArticleViewModel(1,
+        'https://i.picsum.photos/id/237/700/400.jpg', 
+        'Luã Falcão',
+        '28/12/2019', 
+        'Design Patterns #5: Abstract Factory',
+        'Continuando a série sobre padrões de projeto, onde abordamos soluções reutilizáveis e testáveis para problemas frequentes do dia a dia do programador, hoje falaremos sobre o Abstract Factory, um dos cinco padrões criacionais catalogados pela Gang of Four em seu livro Design Patterns: Elements of Reusable Object-Oriented Software.'),
+
+    this.articles = [
+      new ArticleViewModel(1,
+             'https://i.picsum.photos/id/237/700/400.jpg', 
+             'Luã Falcão',
+             '28/12/2019', 
+             'Design Patterns #4: State',
+             'O padrão de projetos State permite que mudemos o comportamento de um objeto baseado na mudança do seu estado, representando cada estado como uma classe, o que reduz a necessidade de usarmos lógica condicional para verificar, por exemplo, o valor do status de um usuário no sistema.'),
+             
+             new ArticleViewModel(2,
+              'https://i.picsum.photos/id/237/700/400.jpg', 
+              'Luã Falcão',
+              '21/12/2019', 
+              'Criando componentes no Angular',
+              ''),
+
+              new ArticleViewModel(3,
+                'https://i.picsum.photos/id/237/700/400.jpg', 
+                'Luã Falcão',
+                '07/11/2019', 
+                'Autenticação JWT com ASP .NET Core API',
+                '')
+    ];
+   }
 
   ngOnInit() {
   }
--- src/app/viewModels/article.viewModel.ts
+++ src/app/viewModels/article.viewModel.ts
@@ -3,8 +3,17 @@ export class ArticleViewModel {
     public title: string;
     public preview: string;
     public creationDate: string;
-    public publishedData: string;
+    public publishedDate: string;
     public content: string;
     public image: string;
-    public autor: string;
+    public author: string;
+
+    constructor(id: number, image: string, author: string, publishedDate: string, title: string, preview: string) {
+        this.id = id;
+        this.image = image;
+        this.author = author;
+        this.publishedDate = publishedDate;
+        this.title = title;
+        this.preview = preview;
+    }
 }
