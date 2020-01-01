a47846fdf67700a6038533d2835db157bc5e567f
Luã Falcão <falcaodeveloper@outlook.com>
2020-01-01 15:43:14 -0300

    Include home panel page to make edition of content

--- src/app/panel/panel.module.ts
+++ src/app/panel/panel.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PanelComponent } from './panel.component';
 import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home/home/home.component';
 
 
 
 @NgModule({
-  declarations: [PanelComponent],
+  declarations: [PanelComponent, HomeComponent],
   imports: [
     CommonModule,
     SharedModule
