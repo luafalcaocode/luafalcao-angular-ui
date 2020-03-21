001185be1d2f7790101cb851f9772fe2bf4a54cc
Luã Falcão <falcaodeveloper@outlook.com>
2020-03-21 15:56:56 -0300

    Implemented a new component to make text move itself

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  palavra: number = 0;
+  nome: string = ' ';
+  titulo: string;
+  titulos: string[] = [
+    ' Engenheiro de Software',
+    ' Programador Web Full Stack',
+    ' Analista de Sistemas'   
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    this.escrever();
   }
 
+  escrever() {
+    let i = 0;
+    
+    let interval = setInterval(() => {
+      this.titulo = this.titulos[this.palavra];
+      if (i < this.titulo.length) {
+        this.nome += this.titulo[i++];
+      }
+      else {
+        clearInterval(interval);
+        setTimeout(() => this.apagar(), 2000);
+      }    
+
+    }, 100);
+  }
+
+  apagar() {
+    let flag = 0;
+    let array = this.nome.split('');
+    let interval = setInterval(() => {
+      if (flag < this.titulo.length) {
+        array.pop();
+        this.nome = array.join('');
+        flag++;
+      }
+      else {
+        clearInterval(interval);
+        this.palavra = (this.palavra == this.titulos.length - 1) ? 0 : ++this.palavra;
+        this.escrever();
+      }
+    }, 50)
+  }
 }
